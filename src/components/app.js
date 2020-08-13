bd8c64cc9ee5763dea5fdec4ed26a87d355808c1
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-13 02:42:02 -0700

    check if data is stored in cookie

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 
@@ -13,6 +14,39 @@ export default class App extends Component {
     };
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.checkLoginStatus = this.checkLoginStatus.bind(this);
+  }
+
+  checkLoginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then(response => {
+        if (
+          response.data.logged_in &&
+          this.state.loggedInStatus === "NOT_LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: response.data.user
+          });
+        } else if (
+          !response.data.logged_in &&
+          this.state.loggedInStatus === "LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+          });
+        }
+        console.log(response);
+      })
+      .catch(error => {
+        console.log("check login error", error);
+      });
+  };
+
+  componentDidMount() {
+    this.checkLoginStatus();
   }
 
   handleLogin = data => {
@@ -21,6 +55,7 @@ export default class App extends Component {
       user: data.user
     });
   };
+
   render() {
     return (
       <div className="app">
