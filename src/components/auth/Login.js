c383d1bce6d620d0d9811e6219c6e1ae0eca77da
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-13 01:57:53 -0700

    Create Login.js

---
+++ src/components/auth/Login.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit = e => {
+    const { email, password } = this.state;
+    axios
+      .post(
+        "http://localhost:3001/sessions",
+        {
+          user: {
+            email: email,
+            password: password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.logged_in) {
+          this.props.handleSuccessfullAuth(response.data);
+        }
+        console.log("Response result", response);
+      })
+      .catch(error => {
+        console.log("Login Errors", error);
+      });
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
